6db7d67b51a3f985774a10744e70b5dca4a90226
nomu0719 <kei53415@gmail.com>
2021-04-05 02:47:58 +0000

    課題３終了

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-
+ 
   resources :users, only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 end
